c5bfa4bebe355894aff648fd2bc07c48d062a5ce
AlejandroJimenez1216 <alejandrojj06@gmail.com>
2026-09-23 20:40:45 -0400

    Updated stats send message and urlPath encoding

--- shortenerRouter.js
+++ shortenerRouter.js
@@ -3,29 +3,31 @@ const hash = require('./hash');
 const Shortener = require('./shortenerModel');
 const router = express.Router();
 process.loadEnvFile();
+const urlPath = `http://localhost:${process.env.PORT}/`;
 
 router.post('/shorten', async (req, res) => {
   if (!req.body.originalUrl) return res.status(404).json({ error: "No URL Found" });
   const shortId = req.body.customAlias ? req.body.customAlias : hash(req.body.originalUrl);
   const idExists = await Shortener.exists({ shortId: shortId }).exec();
-  if (idExists) return res.json({ message: "Alias exists.", link: `http://localhost:3000/${shortId}` });
+  if (idExists) return res.json({ message: "Alias exists.", link: urlPath+shortId });
   const shortener = new Shortener({
     shortId: shortId,
     originalUrl: req.body.originalUrl,
   });
   await shortener.save();
-  res.send(`http://localhost:3000/${shortId}`);
+  res.send(urlPath+shortId);
 });
 
 router.get('/:code/stats/', async (req, res) => {
   try {
     const doc = await Shortener.findOne({ shortId: req.params.code }).exec();
     if (!doc) res.status(404).json({ error: "No Document Found" });
-    res.send(doc.count);
+    res.send(`Number of clicks for ${urlPath+ doc.shortId}: ${doc.count}`);
   } catch {
     return res.status(500).json({ error: "Server error" });
   }
 });
+
 router.get('/:code', async (req, res) => {
   try {
     const doc = await Shortener.findOne({ shortId: req.params.code }).exec();
